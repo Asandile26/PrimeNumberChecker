aabeb0c1172b908fc1bd383199f31cd819b5ea39
Asandile <asandilelangeni@gmail.com>
2025-01-31 14:53:01 +0200

    Code pushed

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const numberInput = document.getElementById("numberInput");
+    const checkBtn = document.getElementById("checkButton");
+    const resultMessage = document.getElementById("resultMessage");
+
+    checkBtn.addEventListener("click", function(){
+
+        const number = parseInt(numberInput.value);
+
+        if(isNaN(number)){
+            resultMessage.textContent = "Please enter a valid number";
+            resultMessage.style.color = "red";
+            return;
+        } 
+        
+        if(isPrime(number)){
+            resultMessage.textContent = `${number} is a prime number`;
+            resultMessage.style.color = "green";
+        }else{
+            resultMessage.textContent = `${number} is not aprime number`;
+            resultMessage.style.color = "red";
+        }
+    });
+
+    function isPrime(num){
+       
+        if(num <= 1) return false;
+        if(num <= 3) return true;
+
+        if(num % 2 === 0 || num % 3 === 0) return false;
+
+        return true;
+        
+        
+    }
+});
